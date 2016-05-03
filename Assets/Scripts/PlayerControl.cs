dda3c1cd306151b1d7827fab7f12e1ec7a23e3bc
lasse <lasse@turborilla.com>
2016-05-03 10:57:17 +0200

    git ignore

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -128,31 +128,31 @@ public class PlayerControl : MonoBehaviour
 	}
 
 	private void updatePickupTile() {
-		canPickup = false;
-
-		float xInput = Input.GetAxisRaw ("Horizontal");
-		float zInput = Input.GetAxisRaw ("Vertical");
-
-		Vector3Int playerPos = levelManager.toTilePosition( this.transform.position );
-		Vector3Int pickupTestPos = playerPos + new Vector3Int ((int)xInput, 0, (int)zInput);
-
-		if( levelManager.getTile(pickupTestPos) != null && Mathf.Abs(xInput) + Mathf.Abs(zInput) < 1.5f ) {
-			Interactable interactable = levelManager.getTile(pickupTestPos).GetComponent<Interactable>();
-			if( interactable.IsPortable ) {
-				pickupAim = pickupTestPos;
-				canPickup = true;
-			}
-		}
-
-
-		if (canPickup) {
-			tileMarker.GetComponent<MeshRenderer> ().enabled = true;
-			tileMarker.transform.position = levelManager.getTileCenter(pickupAim) + new Vector3(0f, 0.5f, 0f);
-		} else {
-			print ("resetting pickup time");
-			timeTillPickup = pickupTime;
-			tileMarker.GetComponent<MeshRenderer>().enabled = false;
-		}
+//		canPickup = false;
+//
+//		float xInput = Input.GetAxisRaw ("Horizontal");
+//		float zInput = Input.GetAxisRaw ("Vertical");
+//
+//		Vector3Int playerPos = levelManager.toTilePosition( this.transform.position );
+//		Vector3Int pickupTestPos = playerPos + new Vector3Int ((int)xInput, 0, (int)zInput);
+//
+//		if( levelManager.getTile(pickupTestPos) != null && Mathf.Abs(xInput) + Mathf.Abs(zInput) < 1.5f ) {
+//			Interactable interactable = levelManager.getTile(pickupTestPos).GetComponent<Interactable>();
+//			if( interactable.IsPortable ) {
+//				pickupAim = pickupTestPos;
+//				canPickup = true;
+//			}
+//		}
+//
+//
+//		if (canPickup) {
+//			tileMarker.GetComponent<MeshRenderer> ().enabled = true;
+//			tileMarker.transform.position = levelManager.getTileCenter(pickupAim) + new Vector3(0f, 0.5f, 0f);
+//		} else {
+//			print ("resetting pickup time");
+//			timeTillPickup = pickupTime;
+//			tileMarker.GetComponent<MeshRenderer>().enabled = false;
+//		}
 	}
 
 	private void setWeaponActive(int weaponIndex, bool active ) {
@@ -195,120 +195,120 @@ public class PlayerControl : MonoBehaviour
 	 * 
 	 */
 	private void updateVelocity() {
-		//Movement
-		float speed = pickupObject == null ? moveSpeed : moveSpeedPickup;
-
-
-		float xInput = Input.GetAxisRaw ("Horizontal");
-		float zInput = Input.GetAxisRaw ("Vertical");
-		
-		GetComponent<Rigidbody>().velocity = Vector3.Normalize(new Vector3 (xInput, 0f, zInput));
-		GetComponent<Rigidbody>().velocity *= speed; 
-
-		float slideDistanceFromBlock = 0.05f;
-		float noSlideDistanceFromBlockCenter = 0.05f;
-		
-		//tile space
-		Vector3 tileSpace = levelManager.toTileSpacePosition (this.gameObject.transform.position);
-		
-		//tiles
-		Vector3Int tilePosition = levelManager.toTilePosition (this.gameObject.transform.position);
-
-		//Dont add velocity if object aheat is big and can be picked up (read is a bomb)
-		Vector3Int tileAhead = tilePosition + new Vector3Int ((int)xInput, 0, (int)zInput);
-		if ( ! ( canPickup && pickupAim.x == tileAhead.x && pickupAim.z == tileAhead.z) ) {
-
-
-			//Add velocity to help player navigate obstacles
-			Vector3Int t = tilePosition + new Vector3Int (-1, 0, 1);
-			bool nwFloor = levelManager.getTile (tilePosition + new Vector3Int (-1, 0, 1)	) == null; 
-			bool nnFloor = levelManager.getTile (tilePosition + new Vector3Int (0, 0, 1)	) == null;
-			bool neFloor = levelManager.getTile (tilePosition + new Vector3Int (1, 0, 1)	) == null;
-			
-			bool wwFloor = levelManager.getTile (tilePosition + new Vector3Int (-1, 0, 0)	) == null;
-			bool ccFloor = levelManager.getTile (tilePosition + new Vector3Int (0, 0, 0)	) == null;
-			bool eeFloor = levelManager.getTile (tilePosition + new Vector3Int (1, 0, 0)	) == null;
-			
-			bool swFloor = levelManager.getTile (tilePosition + new Vector3Int (-1, 0, -1)	) == null;
-			bool ssFloor = levelManager.getTile (tilePosition + new Vector3Int (0, 0, -1)	) == null;
-			bool seFloor = levelManager.getTile (tilePosition + new Vector3Int (1, 0, -1)	) == null;
-			
-			if (zInput > 0.5f){ // pressing north ***
-				if( tileSpace.z > 0.5 - slideDistanceFromBlock ) { // north
-					
-					// FB
-					// FO
-					if(xInput < 0.5f && !nnFloor && nwFloor && wwFloor && tileSpace.x < 0.5f - noSlideDistanceFromBlockCenter ) { //blocked n, free way to w
-						GetComponent<Rigidbody>().velocity = new Vector3(-speed, 0f, GetComponent<Rigidbody>().velocity.z);
-					}
-					
-					// BF
-					// OF
-					if( xInput > -0.5f && !nnFloor && neFloor && eeFloor && tileSpace.x > 0.5f + noSlideDistanceFromBlockCenter ) { //blocked n, free way to e
-						GetComponent<Rigidbody>().velocity = new Vector3(speed, 0f, GetComponent<Rigidbody>().velocity.z);
-					}
-					
-					//BF   FB
-					// O   O 
-					if( nnFloor && ( ( !nwFloor && tileSpace.x < 0.5f && xInput > -0.5f) || ( !neFloor && tileSpace.x > 0.5f && xInput < 0.5f) ) ) { //blocked ne, free way n OR 
-						GetComponent<Rigidbody>().velocity = new Vector3(speed * (0.5f - tileSpace.x) * 4f, 0f, GetComponent<Rigidbody>().velocity.z);
-					}
-				}
-			}
-			
-			if (zInput < -0.5f){ // pressing south
-				if( tileSpace.z < 0.5 + slideDistanceFromBlock ) { // north
-					
-					if( xInput < 0.5f && !ssFloor && swFloor && wwFloor && tileSpace.x < 0.5f - noSlideDistanceFromBlockCenter ) { //blocked n, free way to w
-						GetComponent<Rigidbody>().velocity = new Vector3(-speed, 0f, GetComponent<Rigidbody>().velocity.z);
-					}
-					
-					if( xInput > -0.5f && !ssFloor && seFloor && eeFloor && tileSpace.x > 0.5f + noSlideDistanceFromBlockCenter ) { //blocked n, free way to e
-						GetComponent<Rigidbody>().velocity = new Vector3(speed, 0f, GetComponent<Rigidbody>().velocity.z);
-					}
-					
-					if( ssFloor && ( ( !swFloor && tileSpace.x < 0.5f && xInput > -0.5f) || ( !seFloor && tileSpace.x > 0.5f && xInput < 0.5f) ) ) { //blocked ne, free way n OR 
-						GetComponent<Rigidbody>().velocity = new Vector3(speed * (0.5f - tileSpace.x) * 4f, 0f, GetComponent<Rigidbody>().velocity.z);
-					}
-				}
-			}
-			
-			if (xInput > 0.5f){ // pressing east
-				if( tileSpace.x > 0.5f - slideDistanceFromBlock ) {
-					
-					if(zInput < 0.5f && !eeFloor && seFloor && ssFloor && tileSpace.z < 0.5f - noSlideDistanceFromBlockCenter ) { //blocked n, free way to w
-						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, -speed);
-					}
-					
-					if( zInput > -0.5f && !eeFloor && neFloor && nnFloor && tileSpace.z > 0.5f + noSlideDistanceFromBlockCenter ) { //blocked n, free way to e
-						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, speed);
-					}
-					
-					if( eeFloor && ( ( !seFloor && tileSpace.z < 0.5f && zInput > -0.5f) || ( !neFloor && tileSpace.z > 0.5f && zInput < 0.5f) ) ) { //blocked ne, free way n OR 
-						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, speed * (0.5f - tileSpace.z) * 4f );
-					}
-				}
-			}
-			
-			
-			if (xInput < -0.5f){ // pressing west
-				if( tileSpace.x < 0.5f + slideDistanceFromBlock ) {
-					
-					if( zInput < 0.5f && !wwFloor && swFloor && ssFloor && tileSpace.z < 0.5f - noSlideDistanceFromBlockCenter ) { //blocked n, free way to w
-						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, -speed);
-					}
-					
-					if( zInput > -0.5f && !wwFloor && nwFloor && nnFloor && tileSpace.z > 0.5f + noSlideDistanceFromBlockCenter ) { //blocked n, free way to e
-						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, speed);
-					}
-					
-					if( wwFloor && ( ( !swFloor && tileSpace.z < 0.5f && zInput > -0.5f) || ( !nwFloor && tileSpace.z > 0.5f && zInput < 0.5f) ) ) { //blocked ne, free way n OR 
-						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, speed * (0.5f - tileSpace.z) * 4f );
-					}
-				}
-			}
-		}
-		GetComponent<Rigidbody>().velocity = new Vector3 (Mathf.Clamp(GetComponent<Rigidbody>().velocity.x, -speed, speed ), 0f, Mathf.Clamp(GetComponent<Rigidbody>().velocity.z, -speed, speed ));
+//		//Movement
+//		float speed = pickupObject == null ? moveSpeed : moveSpeedPickup;
+//
+//
+//		float xInput = Input.GetAxisRaw ("Horizontal");
+//		float zInput = Input.GetAxisRaw ("Vertical");
+//		
+//		GetComponent<Rigidbody>().velocity = Vector3.Normalize(new Vector3 (xInput, 0f, zInput));
+//		GetComponent<Rigidbody>().velocity *= speed; 
+//
+//		float slideDistanceFromBlock = 0.05f;
+//		float noSlideDistanceFromBlockCenter = 0.05f;
+//		
+//		//tile space
+//		Vector3 tileSpace = levelManager.toTileSpacePosition (this.gameObject.transform.position);
+//		
+//		//tiles
+//		Vector3Int tilePosition = levelManager.toTilePosition (this.gameObject.transform.position);
+//
+//		//Dont add velocity if object aheat is big and can be picked up (read is a bomb)
+//		Vector3Int tileAhead = tilePosition + new Vector3Int ((int)xInput, 0, (int)zInput);
+//		if ( ! ( canPickup && pickupAim.x == tileAhead.x && pickupAim.z == tileAhead.z) ) {
+//
+//
+//			//Add velocity to help player navigate obstacles
+//			Vector3Int t = tilePosition + new Vector3Int (-1, 0, 1);
+//			bool nwFloor = levelManager.getTile (tilePosition + new Vector3Int (-1, 0, 1)	) == null; 
+//			bool nnFloor = levelManager.getTile (tilePosition + new Vector3Int (0, 0, 1)	) == null;
+//			bool neFloor = levelManager.getTile (tilePosition + new Vector3Int (1, 0, 1)	) == null;
+//			
+//			bool wwFloor = levelManager.getTile (tilePosition + new Vector3Int (-1, 0, 0)	) == null;
+//			bool ccFloor = levelManager.getTile (tilePosition + new Vector3Int (0, 0, 0)	) == null;
+//			bool eeFloor = levelManager.getTile (tilePosition + new Vector3Int (1, 0, 0)	) == null;
+//			
+//			bool swFloor = levelManager.getTile (tilePosition + new Vector3Int (-1, 0, -1)	) == null;
+//			bool ssFloor = levelManager.getTile (tilePosition + new Vector3Int (0, 0, -1)	) == null;
+//			bool seFloor = levelManager.getTile (tilePosition + new Vector3Int (1, 0, -1)	) == null;
+//			
+//			if (zInput > 0.5f){ // pressing north ***
+//				if( tileSpace.z > 0.5 - slideDistanceFromBlock ) { // north
+//					
+//					// FB
+//					// FO
+//					if(xInput < 0.5f && !nnFloor && nwFloor && wwFloor && tileSpace.x < 0.5f - noSlideDistanceFromBlockCenter ) { //blocked n, free way to w
+//						GetComponent<Rigidbody>().velocity = new Vector3(-speed, 0f, GetComponent<Rigidbody>().velocity.z);
+//					}
+//					
+//					// BF
+//					// OF
+//					if( xInput > -0.5f && !nnFloor && neFloor && eeFloor && tileSpace.x > 0.5f + noSlideDistanceFromBlockCenter ) { //blocked n, free way to e
+//						GetComponent<Rigidbody>().velocity = new Vector3(speed, 0f, GetComponent<Rigidbody>().velocity.z);
+//					}
+//					
+//					//BF   FB
+//					// O   O 
+//					if( nnFloor && ( ( !nwFloor && tileSpace.x < 0.5f && xInput > -0.5f) || ( !neFloor && tileSpace.x > 0.5f && xInput < 0.5f) ) ) { //blocked ne, free way n OR 
+//						GetComponent<Rigidbody>().velocity = new Vector3(speed * (0.5f - tileSpace.x) * 4f, 0f, GetComponent<Rigidbody>().velocity.z);
+//					}
+//				}
+//			}
+//			
+//			if (zInput < -0.5f){ // pressing south
+//				if( tileSpace.z < 0.5 + slideDistanceFromBlock ) { // north
+//					
+//					if( xInput < 0.5f && !ssFloor && swFloor && wwFloor && tileSpace.x < 0.5f - noSlideDistanceFromBlockCenter ) { //blocked n, free way to w
+//						GetComponent<Rigidbody>().velocity = new Vector3(-speed, 0f, GetComponent<Rigidbody>().velocity.z);
+//					}
+//					
+//					if( xInput > -0.5f && !ssFloor && seFloor && eeFloor && tileSpace.x > 0.5f + noSlideDistanceFromBlockCenter ) { //blocked n, free way to e
+//						GetComponent<Rigidbody>().velocity = new Vector3(speed, 0f, GetComponent<Rigidbody>().velocity.z);
+//					}
+//					
+//					if( ssFloor && ( ( !swFloor && tileSpace.x < 0.5f && xInput > -0.5f) || ( !seFloor && tileSpace.x > 0.5f && xInput < 0.5f) ) ) { //blocked ne, free way n OR 
+//						GetComponent<Rigidbody>().velocity = new Vector3(speed * (0.5f - tileSpace.x) * 4f, 0f, GetComponent<Rigidbody>().velocity.z);
+//					}
+//				}
+//			}
+//			
+//			if (xInput > 0.5f){ // pressing east
+//				if( tileSpace.x > 0.5f - slideDistanceFromBlock ) {
+//					
+//					if(zInput < 0.5f && !eeFloor && seFloor && ssFloor && tileSpace.z < 0.5f - noSlideDistanceFromBlockCenter ) { //blocked n, free way to w
+//						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, -speed);
+//					}
+//					
+//					if( zInput > -0.5f && !eeFloor && neFloor && nnFloor && tileSpace.z > 0.5f + noSlideDistanceFromBlockCenter ) { //blocked n, free way to e
+//						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, speed);
+//					}
+//					
+//					if( eeFloor && ( ( !seFloor && tileSpace.z < 0.5f && zInput > -0.5f) || ( !neFloor && tileSpace.z > 0.5f && zInput < 0.5f) ) ) { //blocked ne, free way n OR 
+//						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, speed * (0.5f - tileSpace.z) * 4f );
+//					}
+//				}
+//			}
+//			
+//			
+//			if (xInput < -0.5f){ // pressing west
+//				if( tileSpace.x < 0.5f + slideDistanceFromBlock ) {
+//					
+//					if( zInput < 0.5f && !wwFloor && swFloor && ssFloor && tileSpace.z < 0.5f - noSlideDistanceFromBlockCenter ) { //blocked n, free way to w
+//						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, -speed);
+//					}
+//					
+//					if( zInput > -0.5f && !wwFloor && nwFloor && nnFloor && tileSpace.z > 0.5f + noSlideDistanceFromBlockCenter ) { //blocked n, free way to e
+//						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, speed);
+//					}
+//					
+//					if( wwFloor && ( ( !swFloor && tileSpace.z < 0.5f && zInput > -0.5f) || ( !nwFloor && tileSpace.z > 0.5f && zInput < 0.5f) ) ) { //blocked ne, free way n OR 
+//						GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, speed * (0.5f - tileSpace.z) * 4f );
+//					}
+//				}
+//			}
+//		}
+//		GetComponent<Rigidbody>().velocity = new Vector3 (Mathf.Clamp(GetComponent<Rigidbody>().velocity.x, -speed, speed ), 0f, Mathf.Clamp(GetComponent<Rigidbody>().velocity.z, -speed, speed ));
 	
 	}
 
