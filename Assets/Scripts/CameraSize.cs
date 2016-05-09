45773776899b9b17c00c277a67a169966a9fbd33
lasse <lasse@turborilla.com>
2016-05-09 16:49:44 +0200

    Make scene orthographic

--- Assets/Scripts/CameraSize.cs
+++ Assets/Scripts/CameraSize.cs
@@ -3,22 +3,42 @@ using System.Collections;
 
 public class CameraSize : MonoBehaviour {
 
-    public float width = 10f;
+    public float levelWidth = 10f;
     Camera cam;
 
-    public void Update() {
-        if (cam == null) {
-            cam = GetComponent<Camera>();
-        }
-        float requiredCameraDistance = GetRequiredDistance(cam.fieldOfView, cam.aspect, width);
-        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, -requiredCameraDistance);
-    }
 
-    //fovY in degrees
-    public static float GetRequiredDistance(float fieldOfViewY, float aspect, float width) {
-        float fovY = Mathf.Deg2Rad * fieldOfViewY;
-        float fovX = 2f * Mathf.Atan(aspect * Mathf.Tan(fovY / 2f));
-        float distanceFovX = (0.5f * width) / Mathf.Tan(0.5f * fovX);
-        return distanceFovX;
-    }
+	// orthographic
+	public void Update() {
+		if (cam == null) {
+			cam = GetComponent<Camera>();
+		}
+		float aspect = (float)Screen.width / (float)Screen.height;
+		float height = levelWidth / aspect;
+		cam.orthographicSize = height * 0.5f;
+	}
+
+//	//fovY in degrees
+//	public static float GetRequiredDistance(float fieldOfViewY, float aspect, float width) {
+//		float fovY = Mathf.Deg2Rad * fieldOfViewY;
+//		float fovX = 2f * Mathf.Atan(aspect * Mathf.Tan(fovY / 2f));
+//		float distanceFovX = (0.5f * width) / Mathf.Tan(0.5f * fovX);
+//		return distanceFovX;
+//	}
+
+//	// perspective
+//    public void Update() {
+//        if (cam == null) {
+//            cam = GetComponent<Camera>();
+//        }
+//        float requiredCameraDistance = GetRequiredDistance(cam.fieldOfView, cam.aspect, levelWidth);
+//        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, -requiredCameraDistance);
+//    }
+//
+//    //fovY in degrees
+//    public static float GetRequiredDistance(float fieldOfViewY, float aspect, float width) {
+//        float fovY = Mathf.Deg2Rad * fieldOfViewY;
+//        float fovX = 2f * Mathf.Atan(aspect * Mathf.Tan(fovY / 2f));
+//        float distanceFovX = (0.5f * width) / Mathf.Tan(0.5f * fovX);
+//        return distanceFovX;
+//    }
 }
