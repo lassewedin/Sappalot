60cec38211b866b70c21c6b6c4b235ccd78accc0
lasse <lasse@turborilla.com>
2016-05-10 14:17:47 +0200

    Add HurtZone and KillZone

---
+++ Assets/Scripts/HpText.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HpText : MonoBehaviour {
+
+	public void setHp(int hp) {
+		GetComponent<Text>().text = "HP: " + hp;
+	}
+}
---
+++ Assets/Scripts/PlayerHurt.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class PlayerHurt : MonoBehaviour {
+
+	public int startHP = 100;
+	public int hurtZoneDamage = 1;
+	public int hp { get; private set;} 
+	public HpText hpText;
+
+	// Use this for initialization
+	void Start () {
+		hp = startHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider collider) 
+	{
+		if (collider.tag == "HurtZone") {
+			Debug.Log("Player Hurt");
+			hp -= hurtZoneDamage;
+			UpdateHp();
+		}
+
+		if (collider.tag == "KillZone") {
+			Debug.Log("Player Killed");
+			hp -= 10000000;
+			UpdateHp();
+		}
+
+	}
+
+	private void UpdateHp() {
+		hpText.setHp(Math.Max(hp, 0));
+		if (hp <= 0) {
+			Debug.Log("Game Over");
+		}
+	}
+}
