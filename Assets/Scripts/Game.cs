3c3eff4a8d27303ef64d70ae3da2092412f0607e
lasse <lasse@turborilla.com>
2016-05-10 11:51:55 +0200

    Enable game restart

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		if (Input.GetKey(KeyCode.R)) {
+			Debug.Log("Restarting...");
+			SceneManager.LoadScene("MainScene");
+		}
+	}
+}
